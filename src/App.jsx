8abf942e0517a84260b0a0230dda32acc6af1b2f
12PUffS <evgenypech98@gmail.com>
2025-06-06 20:28:42 +0300

    weg

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+// import Header from './components/Header';
+import './App.css';
+
+function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [habits, setHabits] = useState([]);
+
+  const addHabits = () => {
+    if (inputValue.trim()) {
+      setHabits([{ id: Date.now(), text: inputValue }, ...habits]);
+      //         ↑ новый элемент сначала ↑ старые элементы
+      setInputValue('');
+    }
+  };
+
+  const deleteHabits = (id) => {
+    setHabits(habits.filter((habit) => habit.id !== id));
+  };
+
+  return (
+    <div className="container">
+      <div className="input-container">
+        <input
+          onChange={(e) => setInputValue(e.target.value)}
+          type="text"
+          placeholder="Новая привычка"
+          value={inputValue}
+          onKeyPress={(e) => e.key === 'Enter' && addHabits()}
+        />
+        <button className="btn" onClick={addHabits}>
+          Добавить привычку
+        </button>
+        <ul>
+          {habits.map((habit) => (
+            <li key={habit.id}>
+              {habit.text}
+              <span onClick={() => deleteHabits(habit.id)}> ×</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
